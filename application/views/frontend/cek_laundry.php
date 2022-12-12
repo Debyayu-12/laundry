ac5c0fb64c5a96a8dfc2900c8da0c564a97d494c
bunga-as23 <bungaadindasari@gmail.com>
2022-12-12 08:57:08 +0700

    first commit

---
+++ application/views/frontend/cek_laundry.php
@@ -0,0 +1,34 @@
+<form>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-md-10">
+                <input type="text" name="" class="form-control" placeholder="Silahkan masukkan kode transaksi anda!">
+            </div>
+
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-warning">Cek Laundry</button>
+            </div>
+        </div>
+    </div>
+</form>
+
+<div class="container">
+    <table class="table table-bordered table-striped mb-5">
+        <thead>
+            <tr>
+            <th>Nama Konsumen</th>
+            <th>Tanggal Masuk</th>
+            <th>Paket</th>
+            <th>Total</th>
+            <th>Status</th>
+            </tr>
+            
+        </thead>
+
+        <tbody>
+            <tr>
+                <td colspane="5" class="bg-danger text-danger">Tidak ada data</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
